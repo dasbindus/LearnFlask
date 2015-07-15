915a04973c5605ed6ab3a586a340f39e13bdf9c8
Bai Dong <bai123donggood@163.com>
2015-07-15 16:23:46 +0800

    Build a web form by flask-wtf

--- hello.py
+++ hello.py
@@ -1,21 +1,26 @@
 __author__ = 'Baidong'
 
-from flask import Flask
-from flask import request
-from flask import make_response
-from flask import redirect
-from flask import abort
-from flask import render_template
+
+from flask import Flask, render_template, request, make_response, redirect
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
+from flask.ext.wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'CaNYoUSeEMeNoW'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+
+class NameForm(Form):
+    name = StringField('What\'s your name?', validators=[Required()])
+    submit = SubmitField('Submit')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,7 +34,12 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data=''
+    return render_template('index.html', form=form, name=name, current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
