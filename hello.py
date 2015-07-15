9bdab647c175d6fe9801c3e7c5ef842fdfed1af9
Bai Dong <bai123donggood@163.com>
2015-07-15 15:41:10 +0800

    using flask-moment to localize the utc time.

--- hello.py
+++ hello.py
@@ -8,11 +8,14 @@ from flask import abort
 from flask import render_template
 from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -26,7 +29,7 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
