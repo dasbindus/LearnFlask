d096fd33a24a10609d864ebb95d73d5582012e08
Bai Dong <bai123donggood@163.com>
2015-07-15 21:55:39 +0800

    添加flash提醒功能 使用SQLAlchemy链接mysql，通过使用mysql-connector-python模块链接

--- hello.py
+++ hello.py
@@ -1,21 +1,50 @@
 __author__ = 'Baidong'
 
-
-from flask import Flask, render_template, session, request, make_response, redirect, url_for
-from flask.ext.script import Manager
+import os
+from datetime import datetime
+from flask import Flask, render_template, session, request, make_response, redirect, url_for, flash
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
-from datetime import datetime
+from flask.ext.sqlalchemy import SQLAlchemy
+# from sqlalchemy import create_engine
+
+
+DATABASE_NAME = 'test_flask'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CaNYoUSeEMeNoW'
+# this method to create "SQLAlchemy object" by using "mysql-connector-python" module
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:11235813@localhost:3306/test_flask'
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+# use create_engine() to create "engine object"
+# engine = create_engine('mysql+mysqlconnector://root:11235813@localhost:3306/test_flask')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(Form):
@@ -36,6 +65,9 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('You have changed your name.')
         session['name'] = form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
@@ -45,4 +77,5 @@ def user(name):
     return render_template('user.html', name=name)
 
 if __name__ == '__main__':
+    db.create_all()
     manager.run()
