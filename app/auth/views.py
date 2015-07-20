a2883640f06eaf1c8840c225197b63f6a109aeb5
Bai Dong <bai123donggood@163.com>
2015-07-20 17:26:20 +0800

    account condirmation.

--- app/auth/views.py
+++ app/auth/views.py
@@ -12,6 +12,22 @@ from ..emails import send_email
 from .forms import LoginForm, RegistrationForm
 
 
+@auth.before_app_request
+def before_request():
+    if current_user.is_authenticated() \
+            and not current_user.confirmed \
+            and request.endpoint[:5] != 'auth.' \
+            and request.endpoint != ('static'):
+        return redirect(url_for('auth.unconfirmed'))
+
+
+@auth.route('/unconfirmed')
+def unconfirmed():
+    if current_user.is_anonymous() or current_user.confirmed:
+        return redirect(url_for('main.index'))
+    return render_template('auth/unconfirmed.html')
+
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -40,6 +56,32 @@ def register():
             username=form.username.data, 
             password=form.password.data)
         db.session.add(user)
-        flash('You can now login.')
+        db.session.commit() # 提交数据库之后才能赋予新用户 id 值, 而确认令牌需要用到 id,所以不能延后提交
+        token = user.generate_confirmation_token()
+        send_email(user.email, 'Confirm Your Account', 
+            'auth/email/confirm', user=user, token=token)
+        flash('A confirmation email has been send to you by email.')
         return redirect(url_for('auth.login'))
     return render_template('auth/register.html', form=form)
+
+
+@auth.route('/confirm/<token>')
+@login_required
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+    if current_user.confirm(token):
+        flash('You have confirmed your account. Thanks!')
+    else:
+        flash('The confirmation link is invalid or has expired.')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/confirm')
+@login_required
+def resend_confirmation():
+    token = current_user.generate_confirmation_token()
+    send_email(current_user.email, 'Confirm Your Account', 
+            'auth/email/confirm', user=current_user, token=token)
+    flash('A new confirmation email has been send to you by email.')
+    return redirect(url_for('main.index'))
