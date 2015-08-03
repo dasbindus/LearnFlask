23a428a75fb5e14775601de44fb26378290e5f99
Bai Dong <bai123donggood@163.com>
2015-08-03 22:43:16 +0800

    Show follow relationship in profile.

--- config.py
+++ config.py
@@ -20,6 +20,7 @@ class Config:
     MYBLOG_MAIL_SENDER = os.environ.get('MAIL_USERNAME')
     MYBLOG_ADMIN = os.environ.get('MYBLOG_ADMIN')
     MYBLOG_POSTS_PER_PAGE = 20
+    MYBLOG_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
